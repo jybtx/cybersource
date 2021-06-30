a58c01a150ab2e2a5524a0ae6abd18bb9fb4b908
jybtx <2574034626@qq.com>
2021-06-30 15:49:49 +0800

    修改测试地址为正式地址

--- config/cybersource.php
+++ config/cybersource.php
@@ -36,7 +36,7 @@ return [
     "wsdl" => "https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.177.wsdl",
 
     // Modify the URL to point to either a live or test WSDL file with the desired API version for the name-value pairs transaction API.
-    "nvp_wsdl" => "https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_NVP_1.120.wsdl",
+    "nvp_wsdl" => "https://ics2ws.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.177.wsdl",
 
     "name_space" => "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd",
 
